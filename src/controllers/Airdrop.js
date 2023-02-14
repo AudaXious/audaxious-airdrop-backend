4470084e65a8fed870696bdc2f25bd69510bcfba
Sergei <serg.paliy@gmail.com>
2023-02-14 22:25:52 +0700

    update

--- src/controllers/Airdrop.js
+++ src/controllers/Airdrop.js
@@ -2,7 +2,6 @@ const path = require('path');
 const modules = require(path.join(__dirname, '../modules'));
 const config = require(path.join(__dirname, '../config.js')).app;
 const db = require('better-sqlite3')(config.storage.db.main.path);
-const winners = ['cut3an1m3', 'robertrose1991', 'ahallo1990', 'rahul66267247', 'sai999kiran999', 'badboykincry', 'foxrelaxed', 'promsziky10', 'digital_nfts3', 'zulchair3', 'avewoke23', 'eagle_roher', 'dringa8', 'carteirakm', 'leepinggoh', 'bossofficial007', 'ae_tit', 'aniksm98', 'blessings_nfts', 'rajencrypto', 'shashitpatel', 'virusonton', 'arsenal79172545', 'jonatha59738347', 'pauldex_31', 'al2a87', 'olagbadunp', 'alexfcryptoleg', 'whyunhf1', 'prizeehunter', 'james80748083', 'zaktizarnaz', 'arifmaolan', 'jimhasa17591495', 'philipyeoh931', 'ral58068876', 'scaarlethopkins', 'cris__u', 'funky_funky22', 'nihal405994871', 'akshaykumar', 'lukaevans_1', 'mhmmduvysylmz', 'adindadwi5_', 'aishibaeth', 'farhans_stories', 'arieoted', 'harunhariss', 'hmgerardo', 'kraman434'];
 
 exports.get = async(req, res) => {
   const address = req.user.address;
@@ -38,7 +37,7 @@ exports.get = async(req, res) => {
     console.error(`Controllers.Airdrop JSON parse data error, user record id ${user.id}`);
     throw new Error('An error occurred');
   }
-  if (winners.indexOf(twitterData.userName.toLowerCase()) > -1) {
+  if (config.airdrop.winners.indexOf(twitterData.userName.toLowerCase()) > -1) {
     const signature = await modules.signature.signAirdrop(address);
     console.log(signature, address);
     db.prepare('UPDATE users SET signature = :signature WHERE address = :address')
